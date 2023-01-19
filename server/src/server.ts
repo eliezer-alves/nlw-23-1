8c3b8f5d417658d881da810dec46bad77535fdbf
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2023-01-18 21:25:49 -0300

    [server] feat: habits list / habit detail

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,20 @@
-import fastify from "fastify";
-import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+/**
+ * Back-end API RESTful
+ */
 
-const app = fastify();
-const prisma = new PrismaClient();
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
+
+const app = Fastify();
 
 app.register(cors);
+app.register(appRoutes);
 
-app.get("/", async () => {
-  const result = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: "A",
-      },
-    },
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running on port: 3333");
   });
-
-  return result;
-});
-
-app.listen({
-  port: 3333,
-});
